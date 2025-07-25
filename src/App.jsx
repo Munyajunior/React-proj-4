3124289ac69b71a919ff6a57b6f185a2c82d0f49
munyajunior <ivojunior671@gmail.com>
2025-07-25 15:19:50 +0100

    ✨ feat(app): create basic app layout with header and form
    - implement header component with logo and name
    - create form component with input and submit button
    - add basic styling for components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import Header from "../components/Header";
+import Form from "../components/Form";
+import ComplexStateArray from "../components/Complex";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Form />
+      {/* <ComplexStateArray /> */}
+    </>
+  );
+}
