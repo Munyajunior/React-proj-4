6f537c7c64b8aae183d3bdcb19b95346be9502a6
munyajunior <ivojunior671@gmail.com>
2025-07-28 18:04:42 +0100

    ✨ feat(app): integrate pad layout component
    - add pad layout for enhanced ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Form from "../components/Form";
 // import ComplexStateArray from "../components/Complex";
 // import Object from "../components/Object";
+// import PadLayout from "../components/Pad";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Form />
       {/* <ComplexStateArray /> */}
       {/* <Object /> */}
+      {/* <PadLayout DarkMode={true} /> */}
     </>
   );
 }
